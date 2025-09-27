5a4e3cf4650bf506afb43ba81a5911f026f10559
jlaurenzo <pazlaurencejohn@gmail.com>
2025-09-27 17:44:41 +0800

    Create Basic_CalculatorV1.cpp

---
+++ Basic_CalculatorV1.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int oper;
+    float addsum, mulproduct, divq, subdifference, mulplicand, mulplier,
+    minuend, subtrahend, dividend, diviso, addend1, addend2;
+
+    cout << "Welcome to my calculator\n";
+    cout << "You can divide, multiply, add, and subtract! \n\n";
+    cout << "What operation do you want to perform?\n";
+    cout << "Enter [1] for addition\n";
+    cout << "Enter [2] for subtraction\n";
+    cout << "Enter [3] for multiplication\n";
+    cout << "Enter [4] for division\n\n";
+    
+    cout << "ENTER NUMBER: ";
+    cin >> oper;
+    
+    switch (oper)
+    {
+    case 1:
+        cout <<"You chose addition!\n";
+        cout <<"Enter Addend 1: ";
+        cin >> addend1;
+        cout <<"Enter Addend 2: ";
+        cin >> addend2;
+
+        addsum = addend1 + addend2;
+        cout << "Result: " << addsum;
+    break;
+    case 2:
+        cout <<"\nYou chose subtraction!\n";
+        cout <<"Enter minuend: ";
+        cin >> minuend;
+        cout <<"Enter subtrahend: ";
+        cin >> subtrahend;
+
+        subdifference = minuend - subtrahend;
+        cout << "Result: " << subdifference;
+    break;
+    case 3:
+        cout <<"\nYou chose multiplication!\n";
+        cout <<"Enter Mulplicand: ";
+        cin >> mulplicand;
+        cout <<"Enter multiplier: ";
+        cin >> mulplier;
+
+        mulproduct = mulplicand * mulplier;
+        cout << "Result: " << mulproduct;
+    break;
+    case 4:
+        cout <<"\nYou chose division!\n";
+        cout <<"Enter dividend: ";
+        cin >> dividend;
+        cout <<"Enter divisor: ";
+        cin >> diviso;
+
+        divq = dividend / diviso;
+        cout << "Result: " << divq;
+        break;
+    default:
+        cout <<"THANK YOU!";
+    }
+    
+    return 0;
+}
